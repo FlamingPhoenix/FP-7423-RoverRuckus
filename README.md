1e687c4bb6c823da78f3a76acba3f089c9088c0c
hwasteve <steve.e.hwa@gmail.com>
2018-07-20 23:03:22 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+
+FTC Team 7423 - Flaming Phoenix  (2018-19)
+
+Rover Ruckus
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyFirstTeleOp.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+
+@TeleOp(name = "MyFirstTele", group = "none")
+public class MyFirstTeleOp extends OpMode {
+    DcMotor frontLeftWheel;
+    DcMotor frontRightWheel;
+    DcMotor backLeftWheel;
+    DcMotor backRightWheel;
+
+    ///Initializing variables (motors, servos, sensors, and other common variables.
+    ///Remark: Initialization takes plance when user hit "Initialize" on Driver Station
+    @Override
+    public void init() {
+        frontLeftWheel = hardwareMap.dcMotor.get("frontleft");
+        frontRightWheel = hardwareMap.dcMotor.get("frontright");
+        backLeftWheel = hardwareMap.dcMotor.get("backleft");
+        backRightWheel = hardwareMap.dcMotor.get("backright");
+
+        frontRightWheel.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRightWheel.setDirection(DcMotorSimple.Direction.REVERSE);
+    }
+
+
+    @Override
+    public void loop() {
+
+    }
+}
+
+
