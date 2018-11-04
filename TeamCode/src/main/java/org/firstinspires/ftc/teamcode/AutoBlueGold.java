4347dfe33e9f16eed6938bf874e2e4919fd5e0b6
hwasteve <steve.e.hwa@gmail.com>
2018-11-04 10:43:42 -0500

    update to SDK 4.3

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoBlueGold.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoBlueGold.java
@@ -45,7 +45,7 @@ public class AutoBlueGold extends LinearOpMode {
         BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
         parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
         parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
-        parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json";
         parameters.loggingEnabled = true;
         parameters.loggingTag = "IMU";
         parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
