ef07b95b3e171a28c5c198bdaf284ef6ae9b05e0
Cloud9c <kire5030@gmail.com>
2018-11-14 18:39:08 -0500

    changes by Monday meeting

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoBase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoBase.java
@@ -13,13 +13,25 @@ import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
 import org.firstinspires.ftc.teamcode.Library.MyBoschIMU;
+
+import java.util.List;
 
 /**
  * Created by Steve on 7/22/2018.
  */
 
 public abstract class AutoBase extends LinearOpMode {
+    protected static final String TFOD_MODEL_ASSET = "RoverRuckus.tflite";
+    protected static final String LABEL_GOLD_MINERAL = "Gold Mineral";
+    protected static final String LABEL_SILVER_MINERAL = "Silver Mineral";
+
+    protected static final String VUFORIA_KEY = "\"AbYPrgD/////AAAAGbvKMH3NcEVFmPLgunQe4K0d1ZQi+afRLxricyooCq+sgY9Yh1j+bBrd0CdDCcoieA6trLCKBzymC515+Ps/FECtXv3+CTW6fg3/3+nvKZ6QA18h/cNZHg5HYHmghlcCgVUmSzOLRvdOpbS4S+0Y/sWGXwFK0PbuGPSN82w8XPDBoRYSWjAf8GXeitmNSlm9n4swrMoYNpMDuWCDjSm1kWnoErjFA9NuNoFzAgO+C/rYzoYjTJRk40ETVcAsahzatRlP7PJCvNNXiBhE6iVR+x7lFlTZ841xifOIOPkfVc54olC5XYe4A5ZmQ6WFD03W5HHdQrnmKPmkgcr1yqXAJ3rLTK8FZK3KVgbxz3Eeqps0";
+
+    protected enum MineralPosition {LEFT, CENTER, RIGHT, UNKNOWN}
+
     DcMotor fl;
     DcMotor fr;
     DcMotor bl;
@@ -27,7 +39,13 @@ public abstract class AutoBase extends LinearOpMode {
     DriveTrain drivetrain;
     MyBoschIMU imu;
 
-    VuforiaLocalizer vuforia;
+    protected VuforiaTrackable backTarget;
+    protected VuforiaTrackable frontTarget;
+    protected VuforiaTrackable redTarget;
+    protected VuforiaTrackable blueTarget;
+
+    protected VuforiaLocalizer vuforia;
+    protected TFObjectDetector tfod;
 
     public void initialize() {
         fl = hardwareMap.dcMotor.get("frontleft");
@@ -35,10 +53,11 @@ public abstract class AutoBase extends LinearOpMode {
         bl = hardwareMap.dcMotor.get("backleft");
         br = hardwareMap.dcMotor.get("backright");
 
-        fl.setDirection(DcMotorSimple.Direction.REVERSE);
-        bl.setDirection(DcMotorSimple.Direction.REVERSE);
+        fr.setDirection(DcMotorSimple.Direction.REVERSE);
+        br.setDirection(DcMotorSimple.Direction.REVERSE);
 
         drivetrain = new DriveTrain(fl, fr, bl, br);
+        // boolean drivetrain.robotWork = true;
 
         imu = new MyBoschIMU(hardwareMap);
 
@@ -51,6 +70,57 @@ public abstract class AutoBase extends LinearOpMode {
         com.vuforia.Vuforia.setHint(HINT.HINT_MAX_SIMULTANEOUS_IMAGE_TARGETS, 4);
 
         vuforia = ClassFactory.getInstance().createVuforia(param);
+
+        VuforiaTrackables rover = this.vuforia.loadTrackablesFromAsset("RoverRuckus");
+        rover.activate();
+
+        blueTarget = rover.get(0);
+        blueTarget.setName("bluetarget");
+        redTarget = rover.get(1);
+        redTarget.setName("redTarget");
+        frontTarget = rover.get(2);
+        frontTarget.setName("frontTarget");
+        backTarget = rover.get(3);
+        backTarget.setName("backTarget");
+
+        initTfod();
     }
 
+    private void initTfod() {
+        int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
+                "tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
+        tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
+        tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABEL_GOLD_MINERAL, LABEL_SILVER_MINERAL);
+    }
+
+    public MineralPosition getGoldPosition(List<Recognition> recognitions) {
+        int goldMineralX = -1;
+        int silverMineral1X = -1;
+        int silverMineral2X = -1;
+
+        for (Recognition recognition : recognitions) {
+            if (recognition.getLabel().equals(LABEL_GOLD_MINERAL)) {
+                goldMineralX = (int) recognition.getLeft();
+            }
+            else if (silverMineral1X == -1) {
+                silverMineral1X = (int) recognition.getLeft();
+            }
+            else {
+                silverMineral2X = (int) recognition.getLeft();
+            }
+        }
+
+        if (goldMineralX == -1)
+            return MineralPosition.UNKNOWN;
+        else if (goldMineralX != -1)
+        {
+            if (goldMineralX < silverMineral1X)
+                return MineralPosition.LEFT;
+            else if (goldMineralX > silverMineral1X)
+                return MineralPosition.RIGHT;
+        }
+
+        return MineralPosition.UNKNOWN;
+    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoRedSilver.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoRedSilver.java
@@ -1,7 +1,10 @@
 package org.firstinspires.ftc.teamcode;
 
+import android.util.Log;
+
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
+import com.qualcomm.hardware.kauailabs.NavxMicroNavigationSensor;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -10,11 +13,18 @@ import com.vuforia.HINT;
 
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
 import org.firstinspires.ftc.teamcode.Library.MyBoschIMU;
+
+import java.util.List;
 
 /**
  * Created by Steve on 7/22/2018.
@@ -30,35 +40,100 @@ public class AutoRedSilver extends AutoBase {
         VuforiaTrackables rover = this.vuforia.loadTrackablesFromAsset("RoverRuckus");
         rover.activate();
 
-        VuforiaTrackable backTarget = rover.get(2);
-        // 3 = Nebula Picture
-        backTarget.setName("back");
-
         waitForStart();
 
-        /*
-        drivetrain.Turn(0.15F,90,Direction.CLOCKWISE, imu, this);
-        drivetrain.Drive(0.25F, 5, Direction.FORWARD);
-        drivetrain.Turn(
-                0.15F,90,Direction.COUNTERCLOCKWISE, imu, this);
-*/
-        OpenGLMatrix pose = ((VuforiaTrackableDefaultListener)backTarget.getListener()).getPose();
+        MineralPosition goldPosition = MineralPosition.UNKNOWN;;
 
-        /*
-        while (pose == null) {
-            pose = ((VuforiaTrackableDefaultListener)backTarget.getListener()).getPose();
-            bl.setPower(0.15);
-            fl.setPower(0.15);
-            br.setPower(-0.15);
-            fr.setPower(-0.15);
+        if (tfod != null) {
+            tfod.activate();
+
+            List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
+            if (updatedRecognitions.size() == 1)
+            {
+                if (updatedRecognitions.get(0).getLabel().equals(LABEL_SILVER_MINERAL))
+                {
+                    drivetrain.Turn(0.25F, 10, Direction.CLOCKWISE, imu, this);
+                    updatedRecognitions = tfod.getUpdatedRecognitions();
+
+                    if (updatedRecognitions.size() == 1)
+                    {
+                        if (updatedRecognitions.get(0).getLabel().equals(LABEL_SILVER_MINERAL))
+                            goldPosition = MineralPosition.RIGHT;
+                        else
+                            goldPosition = MineralPosition.CENTER;
+                    }
+                    else {
+                        MineralPosition p2 = getGoldPosition(updatedRecognitions);
+
+                        if (p2 == MineralPosition.LEFT)
+                            goldPosition = MineralPosition.CENTER;
+                        else if (p2 == MineralPosition.RIGHT)
+                            goldPosition = MineralPosition.RIGHT;
+                        else
+                            goldPosition = MineralPosition.UNKNOWN;
+                    }
+                }
+                else if (updatedRecognitions.get(0).getLabel().equals(LABEL_GOLD_MINERAL)) {
+                    goldPosition = MineralPosition.LEFT;
+                }
+            }
+            else if (updatedRecognitions.size() == 2)
+            {
+                MineralPosition p1 = getGoldPosition(updatedRecognitions);
+                if (p1 == MineralPosition.LEFT)
+                    goldPosition = MineralPosition.LEFT;
+                else if (p1 == MineralPosition.RIGHT)
+                    goldPosition = MineralPosition.CENTER;
+                else
+                    goldPosition = MineralPosition.RIGHT;
+            }
+
         }
 
-        bl.setPower(0);
-        fl.setPower(0);
-        br.setPower(0);
-        fr.setPower(0);
+        drivetrain.Turn(0.25F, 25, Direction.CLOCKWISE, imu, this);
 
-        */
+        VuforiaTrackableDefaultListener listener = ((VuforiaTrackableDefaultListener) frontTarget.getListener());
+
+        if (listener.isVisible()) {
+
+            OpenGLMatrix pose = listener.getPose();
+            double distance = (pose.getTranslation().get(2)) * 0.0393701;
+
+            Orientation orientation = Orientation.getOrientation(pose, AxesReference.EXTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES);
+
+            OpenGLMatrix rotationMatrix = OpenGLMatrix.rotation(AxesReference.EXTRINSIC, AxesOrder.ZXZ, AngleUnit.DEGREES, orientation.thirdAngle * -1, orientation.firstAngle * -1, 0);
+            OpenGLMatrix adjustedPose = pose.multiplied(rotationMatrix);
+            Orientation adjustedOrientation = Orientation.getOrientation(adjustedPose, AxesReference.EXTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES);
+
+            float imageAngle = Math.abs(adjustedOrientation.secondAngle);
+            double actualDistance = ((distance) / (Math.cos(Math.toRadians(imageAngle))));
+            double innerHeight = (24 * Math.sqrt(2) * Math.sin(Math.toRadians(45 - imageAngle)));
+            double d1 = (24 * Math.sqrt(2) * Math.cos(Math.toRadians(45 - imageAngle)));
+            double d2 = (actualDistance - d1);
+            double mineralAngle = Math.abs(Math.toDegrees(Math.atan((innerHeight) / (d2))));
+
+            telemetry.addData("Gold Position :", "%f", goldPosition);
+
+            if (goldPosition == MineralPosition.RIGHT)
+                drivetrain.Turn(0.25F, (int) mineralAngle, Direction.CLOCKWISE, imu, this);
+            else if (goldPosition == MineralPosition.CENTER) {
+                int centerAngle = ((int) mineralAngle) - 45;
+                if (centerAngle <= 0)
+                    drivetrain.Turn(0.25F, Math.abs(centerAngle), Direction.COUNTERCLOCKWISE, imu, this);
+                else
+                    drivetrain.Turn(0.25F, Math.abs(centerAngle), Direction.CLOCKWISE, imu, this);
+            }
+            else {
+                int leftAngle = ((int) mineralAngle) - 90;
+                if (leftAngle <= 0)
+                    drivetrain.Turn(0.25F, Math.abs(leftAngle), Direction.COUNTERCLOCKWISE, imu, this);
+                else
+                    drivetrain.Turn(0.25F, Math.abs(leftAngle), Direction.CLOCKWISE, imu, this);
+            }
+
+            drivetrain.Drive(0.3f, 12, Direction.FORWARD );
+
+         }
 
         drivetrain.Turn(.3F, 10, Direction.COUNTERCLOCKWISE, imu, this);
         sleep(20000);
