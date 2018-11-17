3c067ce24e0018cb1e25aaa7032126e901372808
Cloud9c <kire5030@gmail.com>
2018-11-16 22:12:30 -0500

    Changes from 11/16/18 Boi this bot goes zoom

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ExperimentIMU.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ExperimentIMU.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -14,6 +15,7 @@ import org.firstinspires.ftc.robotcore.external.ClassFactory;
 
 import org.firstinspires.ftc.teamcode.Library.MyBoschIMU;
 
+@Disabled
 @Autonomous(name = "Expirement", group = "none")
 public class ExperimentIMU extends LinearOpMode {
 
