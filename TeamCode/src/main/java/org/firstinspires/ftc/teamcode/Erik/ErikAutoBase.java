fb1aa4c5566dbcecefb7ff88a827fa5ac89bdf22
Cloud9c <kire5030@gmail.com>
2018-12-11 21:55:01 -0500

    Move TF related methods to autobase for Dec 13 event, and will clean up afterwards

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Erik/ErikAutoBase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Erik/ErikAutoBase.java
@@ -6,6 +6,7 @@ import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
 import com.vuforia.HINT;
 
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
@@ -20,6 +21,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefau
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
 import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
 import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
+import org.firstinspires.ftc.teamcode.Direction;
 import org.firstinspires.ftc.teamcode.DriveTrain;
 import org.firstinspires.ftc.teamcode.Library.MyBoschIMU;
 import org.firstinspires.ftc.teamcode.MyClass.MineralPositionViewModel;
@@ -35,6 +37,11 @@ public abstract class ErikAutoBase extends LinearOpMode {
     protected static final String TFOD_MODEL_ASSET = "RoverRuckus.tflite";
     protected static final String LABEL_GOLD_MINERAL = "Gold Mineral";
     protected static final String LABEL_SILVER_MINERAL = "Silver Mineral";
+
+    protected static final float firstStopAngle = 50f;  // 20 by rough measure
+    protected static final float secondStopAngle = 90f; // 90 by rough measure.
+
+    protected ElapsedTime myruntime = new ElapsedTime();
 
     protected static final String VUFORIA_KEY = "\"AbYPrgD/////AAAAGbvKMH3NcEVFmPLgunQe4K0d1ZQi+afRLxricyooCq+sgY9Yh1j+bBrd0CdDCcoieA6trLCKBzymC515+Ps/FECtXv3+CTW6fg3/3+nvKZ6QA18h/cNZHg5HYHmghlcCgVUmSzOLRvdOpbS4S+0Y/sWGXwFK0PbuGPSN82w8XPDBoRYSWjAf8GXeitmNSlm9n4swrMoYNpMDuWCDjSm1kWnoErjFA9NuNoFzAgO+C/rYzoYjTJRk40ETVcAsahzatRlP7PJCvNNXiBhE6iVR+x7lFlTZ841xifOIOPkfVc54olC5XYe4A5ZmQ6WFD03W5HHdQrnmKPmkgcr1yqXAJ3rLTK8FZK3KVgbxz3Eeqps0";
 
@@ -243,4 +250,195 @@ public abstract class ErikAutoBase extends LinearOpMode {
 
         return positions;
     }
+
+    public void TurnToGold (float power, int angle, Direction d, MyBoschIMU imu, LinearOpMode opMode, long report_time){ // turn to A, B mineral
+
+        long currentTime;
+        int turnAngle;
+        int gold_Found = 0;
+        float distanceFromStart = 0f;
+        //int firstHitEncoderCount = Math.round(1120f * firstHitDistance / (4 * 3.1416f)); //89*3.5 = 312, 0.8 is a factor
+        //int secondHitEncoderCount = Math.round(1120f * secondHitDistance / (4 * 3.1416f)); // 89*15.5 = 1380
+        //int thirdEncoderCount = Math.round(1120f * thirdHitDistance / (4 * 3.1416f)); // 89*27.5 = 2448
+        float startAngle;
+        int currentPosition = 0;
+        int gold_loop_No = 0;
+
+
+        float targetAngle;
+        float currentAngle;
+        float actualPower = power;
+        float stoppingAngle = 0;
+
+        Orientation startOrientation = imu.resetAndStart(d);
+
+        double estimatedAngle;
+        float left_Gold;
+        float right_Gold;
+        float center_Gold;  // center coodinates of gold
+        float current_Gold_Width; // width of gold, to drive towards it.
+
+        myruntime.reset(); // need to use time for tracking minerals instead of just  number of objects
+        currentTime = Math.round(myruntime.milliseconds());
+
+        telemetry.addData("before moving time", currentTime);
+        Log.i("[phoenix]:pretime", Double.toString(currentTime));
+
+        //fr.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        //fl.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        //fr.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        //fl.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        if (d == Direction.COUNTERCLOCKWISE) {
+
+            actualPower = power;
+
+            targetAngle = startOrientation.firstAngle + angle;
+            currentAngle = startOrientation.firstAngle;
+            startAngle = currentAngle;
+
+            while ((gold_Found == 0) && (currentAngle < targetAngle) && opMode.opModeIsActive()) {
+
+                currentAngle = imu.getAngularOrientation().firstAngle;
+
+                opMode.telemetry.addData("start:", startOrientation.firstAngle);
+                opMode.telemetry.addData("current:", currentAngle);
+                opMode.telemetry.addData("target:", targetAngle);
+                opMode.telemetry.update();
+
+
+                //fl.setPower(actualPower);  // first calibrate time on floor..0.12 at dr warners, 0.14 at carpet
+
+                fl.setPower(-(actualPower));
+                fr.setPower(actualPower);
+                bl.setPower(-(actualPower));
+                br.setPower(actualPower);
+
+                currentTime = Math.round(myruntime.milliseconds());
+
+                if (tfod != null) {
+
+                    List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
+                    if (updatedRecognitions != null) {
+                        telemetry.addData("# Object Detected", updatedRecognitions.size());
+                        if (updatedRecognitions.size() <= 3) {
+                            int goldMineralX = -1;
+                            int silverMineral1X = -1;
+                            int silverMineral2X = -1;
+                            for (Recognition recognition : updatedRecognitions) {
+                                if (recognition.getLabel().equals(LABEL_GOLD_MINERAL)) {
+                                    gold_loop_No = gold_loop_No + 1;
+                                    currentTime = Math.round(myruntime.milliseconds());
+                                    telemetry.addData("1st Gold time ", currentTime);
+                                    Log.i("[phoenix]:goldtime", Double.toString(currentTime));
+
+                                    //if (currentTime < (1.5*secondHitTime)) {
+                                    //goldMineralX = (int) recognition.getLeft();
+                                    left_Gold = recognition.getLeft();
+                                    right_Gold = recognition.getRight();
+                                    // here, need to add code to come up with center of gold and use that as control varialble for moving robot towards it..
+                                    center_Gold = (left_Gold + right_Gold) / 2f;
+                                    current_Gold_Width = right_Gold - left_Gold;
+
+                                    telemetry.addData("center of gold ", center_Gold);
+                                    Log.i("[phoenix]:i_goldcenter", Float.toString(center_Gold));
+                                    Log.i("[phoenix]:i_goldleft", Float.toString(left_Gold));
+                                    Log.i("[phoenix]:i_goldright", Float.toString(right_Gold));
+                                    Log.i("[phoenix]:i_width", Float.toString(current_Gold_Width));
+                                    Log.i("[phoenix]:i_Pre Cnt ", Integer.toString(fr.getCurrentPosition()));
+
+                                    // 2018-11-26 Ended the evening not sure what to put for the if statement below.  May need to do hand testing to test left, right and center values
+                                    if ((center_Gold > 80f) && (center_Gold < 640f)) {   // here 300 can change to other numbers, perhaps 400 ?
+                                        //currentTime = Math.round(runtime.milliseconds()); // use this to control position
+                                        currentTime = Math.round(myruntime.milliseconds());
+                                        Log.i("[phoenix]:goldtime ", Double.toString(currentTime));
+                                        erikdrivetrain.StopAll();
+                                        sleep(5000);
+                                        currentPosition = Math.abs(fr.getCurrentPosition());
+                                        telemetry.addData("aft encoder ", currentPosition);
+                                        Log.i("[phoenix]:aft encoder ", Integer.toString(currentPosition));
+                                        telemetry.update();
+                                        if (currentAngle < (startAngle + firstStopAngle + 20) && (gold_Found == 0)) {
+                                            // should try pro-strafe instead of regular strafe, to test navigation control
+
+                                            erikdrivetrain.Strafe(0.30f, 19f,  Direction.RIGHT);
+
+// drivetrain.StopAll();
+//                                              drivetrain.Strafe(0.4f, 25f, Direction.RIGHT);
+//  sleep(500);
+//                                            drivetrain.Strafe(0.4f, 25, Direction.LEFT);
+//                                            drivetrain.StopAll();
+//                                            sleep(500);
+                                            gold_Found = 1; // gold is in A position
+                                            currentPosition = 0;
+                                            //fr.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                                            telemetry.addData("at end of gold loop", "gold 1");
+                                            Log.i("[phoenix]:goldloop", "at end of gold loop 1");
+                                            telemetry.addData("gold frequency", gold_loop_No);
+                                            Log.i("[phoenix]:gold freq", Integer.toString(gold_loop_No));
+                                            telemetry.update();
+                                        } else if (currentAngle < (startAngle + secondStopAngle + 20) && (gold_Found == 0)) {
+                                            erikdrivetrain.Strafe(0.30f, 5.5f, Direction.RIGHT);
+//                                            drivetrain.Strafe(0.4f, 5.5F, Direction.RIGHT);
+//                                            drivetrain.StopAll();
+//                                            sleep(500);
+//                                            drivetrain.Strafe(0.4f, 5.5F, Direction.LEFT);
+//                                            drivetrain.StopAll();
+//                                            sleep(500);
+                                            gold_Found = 3;  // gold is in C position
+                                            currentPosition = 0;
+                                            //fr.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                                            telemetry.addData("at end of gold loop", "gold 3");
+                                            Log.i("[phoenix]:goldloop", "at end of gold loop 3");
+                                            telemetry.addData("gold frequency", gold_loop_No);
+                                            Log.i("[phoenix]:gold freq", Integer.toString(gold_loop_No));
+                                            telemetry.update();
+                                        } else if (gold_Found == 0) {   // gold is at third position..
+
+                                            erikdrivetrain.Strafe(0.30f, 19, Direction.RIGHT);
+//
+//                                             drivetrain.Strafe(0.4f, 14F, Direction.RIGHT);
+//                                            drivetrain.StopAll();
+//                                            sleep(1500);
+//                                            drivetrain.Strafe(0.4f, 14F, Direction.LEFT);
+//                                            drivetrain.StopAll();
+//                                            sleep(1500);                                            //tfod.deactivate();
+                                            gold_Found = 2;  // gold is in B position
+                                            currentPosition = 0;
+                                            //fr.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                                            telemetry.addData("at end of gold loop", "gold 2");
+                                            Log.i("[phoenix]:goldloop", "at end of gold loop 2");
+                                            telemetry.addData("gold frequency", gold_loop_No);
+                                            Log.i("[phoenix]:gold freq", Integer.toString(gold_loop_No));
+                                            telemetry.update();
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    telemetry.update();
+
+                }
+            }
+
+            if (gold_Found == 0) {
+                currentTime = Math.round(myruntime.milliseconds());
+                currentPosition = fr.getCurrentPosition();
+                telemetry.addData("no gold found, current time ", currentTime);
+                telemetry.addData("no gold found, current counter ", currentPosition);
+                telemetry.addData("gold found flag ", gold_Found);
+
+                Log.i("[phoenix]:NoGold, time ", Double.toString(currentTime));
+                Log.i("[phoenix]:NoGold, eCnt ", Integer.toString(currentPosition));
+                Log.i("[phoenix]:Gold Fflag ", Integer.toString(gold_Found));
+                telemetry.update();
+                erikdrivetrain.StopAll();
+            }
+
+        } else {
+        } // direction clockwise..
+    }
+
 }
