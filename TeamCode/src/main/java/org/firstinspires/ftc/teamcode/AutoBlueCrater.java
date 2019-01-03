1bab6e7def2a486d742663994647915d4ff8e458
Cloud9c <kire5030@gmail.com>
2019-01-03 15:20:27 -0500

    minor change to prep for Jan 3 scrimmage: apply same changes from RedDepot to RedCrater, and make blueCrater and BlueDepot same as red side. reduce some sleep time in autobase to save total time.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoBlueCrater.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoBlueCrater.java
@@ -44,100 +44,32 @@ public class AutoBlueCrater extends AutoBase {
             // Wait for the start button
             waitForStart();
 
-
-            // Lower the robot
-
-            // Detach from the lander
+            arm.setPosition(0.1);
+            hopper.setPosition(0.9);
+            // Lower the robot and detach from the lander
+            this.releaseFromLander();
 
             // Prep steps a) Move forward 3 inches, b) strafe, c) turn about 45 degree, ready to scan mineral
+            drivetrain.Drive(0.40f, 3.5f, Direction.FORWARD); //3.5
+            sleep(100);
+            drivetrain.Turn(0.25f, 43, Direction.COUNTERCLOCKWISE, imu, this); // was 46
 
-/////////////////  ////////////////////////////////////////////////////////////////////////////////////////////////
-            //drivetrain.Drive(0.20f, 6.0f, Direction.FORWARD); //3.5
-            //sleep(300);
-            //drivetrain.Strafe(0.25f, 7.0f, Direction.RIGHT );
-            //sleep(300);
-            //drivetrain.Turn(0.25f, 51, Direction.COUNTERCLOCKWISE, imu, this);
-            //sleep(300);
+            sleep(100);
+            this.sampleGold(this);
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            // ??? dont think this line is needed
+            //drivetrain.Strafe(0.6f, 3.5f, Direction.RIGHT);
+            sleep(100);
 
-
-            ////////////alternative-less tested///////
-            // drivetrain.Drive(0.20f, 3.0f, Direction.FORWARD); //3.5
-            // sleep(300);
-            // drivetrain.Turn(0.25f, 35, Direction.COUNTERCLOCKWISE, imu, this);
-            // sleep(300);
-            // drivetrain.Strafe(0.25f, 3.0f, Direction.RIGHT);
-            // sleep(300);
-            // drivetrain.Drive(0.2f, 2.0f, Direction.BACKWARD);
-            // sleep(500);
-////////////////////////fully-tested//////////////////////////////////////////////////////////////////////////////////////
-            drivetrain.Strafe(0.3f, 6.5f, Direction.RIGHT);
-            sleep(300);
-            //drivetrain.Drive(0.2f, 1.5f, Direction.BACKWARD);
-            //sleep(300);
-/////////////////////////////////////////////////////
-
-
-            // scan first mineral
-
-            int detectionOutcome = 0;
-
-            // could have another variable, stop_distance, so that even no GOLD is found, will stop and pause.
-            detectionOutcome = DriveToScanFirstMineral(0.11f, Direction.FORWARD, this); // 0.11 at Dr Warner, 0.15 at carpets
-            sleep(300);
-
-            // Explanation: decide next step based on outcome of first mineral,
-            // if A is gold, hit it anc come back, turn 45 dgree CCW
-            // if A is not Gold, turn 45 degrees CCW, scan Gold Diagonally, once find the gold, hit and come back.
-
-            if (detectionOutcome == 1) { //ScanFirstMineral() == 1
-                telemetry.addData("Gold found", "during first scan");
-                Log.i("[phoenix]:gold detected", "found gold");
-                sleep(300);
-                StrafeWhileVisible(0.4f, 12.5f, 520f, 10, this); // was 10, goldwidth was 460
-                telemetry.addData("Gold aft straf", "after strafe");
-                Log.i("[phoenix]:gold aft str", "after strafe");
-                sleep(300);
-                drivetrain.Strafe(0.3f, 8.5f, Direction.LEFT); // was 6.5
-                sleep(100);
-                drivetrain.Turn(0.4f, 35, Direction.COUNTERCLOCKWISE, imu, this); // shouid be 45, compensate for wheel issue
-                sleep(100);
-                drivetrain.Drive(0.3f, 32f, Direction.FORWARD);}
-            else if (detectionOutcome == 2) { //ScanFirstMineral() == 2, in this scenario, either B or C is GOLD
-                telemetry.addData("Silver found", "during first scan");
-                Log.i("[phoenix]:Silv detected", "found silver");
-                sleep(300);
-                drivetrain.Turn(0.2f, 35, Direction.COUNTERCLOCKWISE, imu, this); // should be 45, compensate for wheels issue
-                telemetry.addData("Silver aft turn", "after turn");
-                Log.i("[phoenix]:Silv aft turn", "aft turn");
-                sleep(300);
-                drivetrain.Drive(0.2f, 3.0f, Direction.BACKWARD);
-                sleep(300);
-                // scan the next two minerals for GOLD
-                scanGold_Diagonal(0.11f, 200, 420, this); // was 240 and 380
-                sleep(100);
-                //drivetrain.Drive(0.3f, 1f, Direction.FORWARD);
-            }
-
-            else {
-                telemetry.addData("no mineral found", "during first scan");
-                telemetry.update();
-                Log.i("[phoenix]: scan result", "no gold");
-            }
-
-
-
-            sleep(200);
-            drivetrain.Turn(0.35f, 52, Direction.COUNTERCLOCKWISE, imu, this);
+            drivetrain.Turn(0.4f, 52, Direction.COUNTERCLOCKWISE, imu, this);
             // then turn to image
-            sleep(300);
+            sleep(100);
             telemetry.addData(" after the turn, before strafe to image", "before strafe to image");
             Log.i("[phoenix]:after turn", "before strafe to image");
             // this is optional, as most likely the robot will see image after above 52 degree turn.
             drivetrain.TurnToImage(0.13f, Direction.COUNTERCLOCKWISE, blueTarget, imu, this);
             //strafe to image
-            drivetrain.StrafeToImage(0.25f, blueTarget, this); // was 0.4
+            drivetrain.StrafeToImage(0.4f, blueTarget, this); // was 0.4
             telemetry.addData(" after the strafe to image", "after strafe to image");
             Log.i("[phoenix]:after strafe", "after strafe to image");
             // this sleep could be tuned to accommodate alliance partner, depending on when they coming to depot and drop their maker
@@ -149,15 +81,14 @@ public class AutoBlueCrater extends AutoBase {
             }
 
             // drive backward for to depot, it was 58
-            drivetrain.Drive(.4f, 53f, Direction.FORWARD);
-            sleep(300);
+            drivetrain.Drive(0.6f, 54.5f, Direction.FORWARD);
+            sleep(100);
             // drop marker
-            markerHook.setPosition(0.1);
-            sleep(300);
-            drivetrain.Drive(.65f, 75, Direction.BACKWARD); // continue to drive to crater
+            markerHook.setPosition(0.0);
+            sleep(100);
+            drivetrain.Drive(.65f, 68f, Direction.BACKWARD); // continue to drive to crater
 
             // end of auto routine.
-            //////////////////////////////////////////////////////////////////////////////////////////////////////////////
         }
 
 
